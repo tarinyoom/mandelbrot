484ff675cf40b53259ab383e495f26ccc4cb2e79
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2024-10-20 16:56:46 -0700

    Remove test infrastructure

--- test.ps1
+++
@@ -1,7 +0,0 @@
-$execPath = Join-Path -Path $PSScriptRoot -ChildPath "build\tests\Release\scarftest.exe"
-
-if (Test-Path -Path $execPath) {
-    & $execPath $args
-} else {
-    Write-Host "Executable not found at '$execPath'. Build the executable first before running it."
-}
